1d856026c466af21fc72c2ea0d470e9bfb925a97
Maaz Ahmed <149291145+MaazAhmed147354@users.noreply.github.com>
2024-12-14 18:30:25 +0500

    Delete Website directory

--- Website/SoftwareDownloadSite/src/pages/Documentation/documentation.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Documentation() {
-    return (
-        <>Dcoumentation</>
-    )
-}
-
-export default Documentation
--- Website/SoftwareDownloadSite/src/pages/Support/support.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Support() {
-    return (
-        <>Support</>
-    )
-}
-
-export default Support
