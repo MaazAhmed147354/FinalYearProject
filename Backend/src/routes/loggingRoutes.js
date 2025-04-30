e89cbb5f6e5d2c8c4696c074cba6e3b44f8ec57e
MaazAhmed147354 <maza147354@gmail.com>
2025-04-30 15:08:22 +0500

    Logout route added

--- Backend/src/routes/loggingRoutes.js
+++ Backend/src/routes/loggingRoutes.js
@@ -3,3 +3,6 @@ const loginController = require('../controllers/loggingController');
 module.exports.loginUser = async (event) => {
     return await loginController.loginUser(event);
 };
+module.exports.logoutUser = async (event) => {
+    return await loginController.logoutUser(event);
+};
